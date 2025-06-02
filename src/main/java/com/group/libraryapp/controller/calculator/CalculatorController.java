5806ff6005ecb24d6c8f032b35fb8508fd1e452d
Salamamder <mpring@naver.com>
2025-06-02 14:39:41 +0900

    POST API 생성 연습

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,7 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // api를 개발하려는 클래스에 적어줘야 하는거임
@@ -13,6 +16,12 @@ public class CalculatorController {
     public int addTwoNumbers(CalculatorAddRequest request) {
         // @RequestParam : 쿼리를 통해서 넘어온 데이터를 받도록 함
         return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply") // POST /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        // @RequestBody 해당 어노테이션을 통해 바디의 json을 처리해서 클래스로 넘겨주게 된다
+        return request.getNumber1() * request.getNumber2();
     }
 }
 
