7cfdd78913c4cba3b9d9de5192904f622b09222e
Salamamder <mpring@naver.com>
2025-06-02 16:12:34 +0900

    GET API를 통해 유저 조회하는 기능 구현
    리스트 안에 json형식으로 저장이 되며
    이는 fori 루프를 통해 인덱스마다
    get 해주고 getName을 거치면
    정보를 조회할 수 있다.

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,6 +2,8 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController // api설정
+// 해당 restcontroller 덕분에 get 요청으로 자료 조회시
+// json형태로 자료를 반환해준다
 public class UserController {
 
-    private  final List<User> users = new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
 
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-    users.add(new User(request.getName(), request.getAge()));
+        users.add(new User(request.getName(), request.getAge()));
+    }
+
+    @GetMapping("/user")
+    public List<UserResponse> getUsers() {
+        List<UserResponse> responses = new ArrayList<>();
+        for (int i = 0; i < users.size(); i++) {
+            responses.add(new UserResponse(i+1, users.get(i)));
+        }
+        return responses;
     }
 }
