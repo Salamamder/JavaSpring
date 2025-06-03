89989e17a934bd7de73be69be39eee1ee1722a85
Salamamder <mpring@naver.com>
2025-06-03 16:06:21 +0900

    자바에서 sql 수정, 삭제 api 작성 동작 확인까지 완료

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,13 +2,11 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -32,28 +30,40 @@ public class UserController {
         jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
+    //    @GetMapping("/user")
+//    public List<UserResponse> getUsers() {
+//        String sql = "SELECT * FROM user";
+//        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+//            @Override
+//            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                long id = rs.getLong("id");
+//                String name = rs.getString("name");
+//                int age = rs.getInt("age");
+//                return new UserResponse(id, name, age);
+//            }
+//        });
+//    }
+// 람다로 바꾼 경우
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
         String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
-            @Override
-            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                int age = rs.getInt("age");
-                return new UserResponse(id, name, age);
-            }
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
         });
     }
-// 람다로 바꾼 경우
-//    @GetMapping("/user")
-//    public List<UserResponse> getUsers() {
-//        String sql = "SELECT * FROM user";
-//        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-//            long id = rs.getLong("id");
-//            String name = rs.getString("name");
-//            int age = rs.getInt("age");
-//            return new UserResponse(id, name, age);
-//        });
-//    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
 }
